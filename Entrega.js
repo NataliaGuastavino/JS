87aae39d2ef5157f808156659b4c24a83b1e9f74
NataliaGuastavino <a.nataliaguastavino@gmail.com>
2023-01-27 17:00:50 +0100

    Segundo commit.

--- Entrega.js
+++ Entrega.js
@@ -1,91 +1,255 @@
-let nombre = prompt("Bienvenido a su compañía de viajes. Por favor, ingrese su nombre.")
 
+let nombre = prompt("Bienvenido a su agencia de turismo. Por favor, ingrese su nombre.")
 let destino = (alert("Bienvenido/a " + nombre + "."))
 
-let cantidadPersonas = parseInt(prompt("Mencione cuántas personas van a realizar el viaje"))
-console.log(`La cantidad de personas que van a viajar son ${cantidadPersonas}`)
-
-let cantidadDias = parseInt(prompt("Mencione la cantidad de días que dispone. Si usted reserva más de 7 dáas, se le aplicará un 10% de descuento en su compra"))
-
-console.log(`La cantidad de días que dispone son ${cantidadDias}. `)
-alert(`La cantidad de días que dispone son ${cantidadDias}.`)
-
-function calcularCosto(precioNoche, cantidadDias, cantidadPersonas) {
-    if (cantidadDias >= 7) {
-        precioTotal = (precioNoche * cantidadDias * cantidadPersonas) * 0.9;
+// Defino mi clase de productos: class constructora + atributos de la clase
+class Actividad{
+    constructor (id, nombreActividad, costo) {
+        this.id = id,
+        this.nombreActividad = nombreActividad,
+        this.costo = costo
     }
-    else {
-        precioTotal = (precioNoche * cantidadDias * cantidadPersonas);
-    }
-    console.log(`El valor de la noche es ${precioNoche}€ , y el costo total de su estadía es de ${precioTotal}€`)
-    alert(`El valor de la noche es ${precioNoche}€ , y el costo total de su estadía es de ${precioTotal}€`)
 }
 
-function mostrarPaises (pais1, pais2) {
-    let pais = prompt(nombre +  `Elija un pais
-            1. ${pais1}
-            2. ${pais2} `)
+// Instanciación de los primeros objetos de productos:
+const campNou = new Actividad (1, "Camp Nou", 70)
+const casaBattlo = new Actividad (2, "Casa Battló", 60)
+const casaMila = new Actividad (3, "Casa Milá", 40)
+const casaPedrera = new Actividad (4, "Casa de la Pedrera", 50)
+const parkGuell = new Actividad (5, "Park Guell", 30)
+const sagradaFamilia = new Actividad (6, "Sagrada Familia", 90)
 
-    return pais;
+
+// Creo el array de actividades y pusheo los objetos ya existentes:
+const listaActividades = []
+listaActividades.push (campNou, casaBattlo, casaMila, casaPedrera, parkGuell, sagradaFamilia)
+
+
+// Creo mi función para ver las actividades disponibles:
+function verActividades (array) {
+    console.log("Nuestra agencia de turismo ofrece las siguientes actividades:")
+    alert (`Nuestra agencia de turismo ofrece las siguientes actividades:
+    ${array.map ( act => {
+        return `
+        ${act.id} - ${act.nombreActividad}, cuyo costo es € ${act.costo}` }).join ('')} `)
+    array.forEach (
+        (act) => {console.log(`${act.id} - ${act.nombreActividad}, cuyo costo es € ${act.costo}`)
+        }
+    )
+}    
+
+// Creo una función para agregar nuevas actividades:
+function agregarActividad (array){
+    let nombreActividad = prompt (`Ingrese el nombre de la actividad que desea agregar`)
+    let costoActividad = prompt (`Ingrese el costo de la nueva actividad`)
+
+    // Válido que el precio ingresado sea un número:
+    while(isNaN(costoActividad)){
+        costoActividad = prompt(`El valor ingresado no es un número válido. Por favor, ingrese un precio en números`)
+    }
+    // Construyo el nuevo producto en base a los parámetros ingresados por el usuario:
+    const nuevaActividad = new Actividad (array.length+1, nombreActividad, costoActividad)
+    // Agrego el nuevo producto al array de productos:
+    array.push (nuevaActividad)
+    // Consultar el catálogo. En caso de no elegir que se muestre, informo que el producto se agregó correctamente.
+    let opcionActividad = prompt(`¿Desea consultar el catálogo actualizado?
+    1 - Sí
+    2 - No`)
+    switch(opcionActividad){
+        case "1":
+        verActividades (listaActividades)
+        break
+        case "2":
+        alert(`La actividad ${nuevaActividad.nombreActividad} fue agregada a la lista exitosamente`)
+        break
+        default:
+        alert(`La actividad ${nuevaActividad.nombreActividad} fue agregada a la lista exitosamente`)            
+        break
+    }
 }
 
-// Variables
-let precioTotal 
-let costo
-let pais
+// Creo funciones para ordenar el array de actividades:
+// Por precio: de menor a mayor
+function precioMenorMayor (array){
+    // Hago una copia del array
+    let menorMayor = [].concat(array)
+    //Ordeno con método Sort
+    menorMayor.sort((a, b) => a.costo - b.costo)
+    verActividades (menorMayor)
+}
 
+// Por precio: de mayor a menor
+function precioMayorMenor(array){
+    // Hago una copia del array
+    let mayorMenor = [].concat(array)
+    // Ordeno con método Sort
+    mayorMenor.sort((a, b) => b.costo - a.costo)
+    verActividades (mayorMenor)
+}
+
+// Alfabéticamente:
+function ordenarAlfabeticamente (array){
+    // Hago una copia del array
+    let ordenAlfa = [].concat(array)
+    //Ordeno con método Sort
+    ordenAlfa.sort((a, b) => {
+        if (a.nombreActividad > b.nombreActividad) {
+        return 1
+        }
+        if (a.nombreActividad < b.nombreActividad) {
+        return -1
+        }
+        // a es igual b
+        return 0
+    })
+    console.log (verActividades (ordenAlfa))
+}
+
+// Creo una función para preguntarle al usuario qué método desea utilizar para ordenar el array de productos:
+function ordenarActividades (array){
+    let actividadesOrdenadas = (prompt (`Indique cómo desea ordenar las actividades
+    1 - Por precio de menor a mayor
+    2 - Por precio de mayor a menor
+    3 - Por orden alfabético`))
+
+    switch(actividadesOrdenadas){
+        case "1":
+        precioMenorMayor (array)
+        break
+        case "2":
+        precioMayorMenor (array)
+        break
+        case "3":
+        ordenarAlfabeticamente (array)
+        break
+        default:
+        alert (`La opción no es válida. Por favor, vuelva a intentarlo`)
+        break
+    }
+}
+
+// Creo una función para eliminar actividades del array:
+function borrarAct (array){
+    // Muestro el catálogo
+    verActividades (array)
+    // Guardo la opción seleccionada por el usuario en una variable
+    let idEliminar = parseInt (prompt(`Seleccione la actividad a eliminar`))
+    // Valido que la opción ingresada sea un número
+    while(isNaN(idEliminar)){
+        idEliminar = prompt(`La opción ingresada es incorrecta. Por favor, ingrese un número del listado.`)
+    }
+    // Valido que el producto a eliminar exista
+    while(idEliminar > array.length){
+        idEliminar = prompt(`Producto inexistente. Ingrese un producto del catálogo`)
+    }
+    // Map: 
+    let arrayId = array.map(a => a.id)
+    // Creo una variable que registre el index del id elegido
+    let indice = arrayId.indexOf(idEliminar)
+    // Splice:
+    array.splice(indice, 1)
+    // muestro el catálogo modificado
+    alert (`El producto ${idEliminar} fue eliminado exitosamente.`)
+    verActividades (array)
+}
+
+// Creo una función para buscar actividades:
+function buscarAct(array){
+    let actBuscada = prompt (`Ingrese la actividad que desea encontrar:`)
+    let actBuscar = array.filter((act) => act.nombreActividad.toLowerCase() == actBuscada.toLowerCase())
+    if(actBuscar.length == 0){
+        alert(`No tenemos ${actBuscada} en nuestra lista de actividades`)
+    } else {
+        verActividades(buscarAct)
+    }
+}
+
+// Creo mi función de menú para realizar tareas interactivas con el usuario:
+function menu(){
 let salirMenu = true
 do {
-    let opcionMenu = prompt(nombre + `, mencione su región de salida
-    1 - Centro América
-    2 - América del Norte y del Sur
-    3 - Europa
-    0 - Salir Menú `)
+    let elegirUsuario = prompt(` ${nombre} por favor, indique si es empleado o cliente:
+    1 - Empleado
+    2 - Cliente
+    3 - Salir del menú`)
 
-    switch (opcionMenu) {
-        case "1":
-            console.log("Centro América")
-            pais = mostrarPaises('Cuba', 'Mexico')
-            switch (pais) {
+    switch (elegirUsuario){
+        case "1": 
+            let menuEmpleado = prompt(`${nombre} usted ingresó como empleado. Seleccione la operación que desea realizar:
+            1 - Consultar actividades.
+            2 - Agregar una nueva actividad.
+            3 - Ordenar actividades.
+            4 - Eliminar una actividad`)
+
+            switch(menuEmpleado){
+                case "1": 
+                    verActividades (listaActividades)
+                break
+
+                case "2": 
+                    agregarActividad (listaActividades)
+                break
+
+                case "3": 
+                    ordenarActividades (listaActividades)
+                break
+
+                case "4": 
+                    borrarAct (listaActividades)
+                break
+
+                default:
+                    alert(`La opción ingresada no es válida. Vuelva a intentarlo desde el principio`)
+                break
+            }
+        break
+
+        case "2": 
+            let menuCliente = prompt(`${nombre} usted ingresó como cliente. Elija qué operación desea realizar:
+            1 - Ver actividades
+            2 - Buscar actividades
+            3 - Agregar actividades al carrito
+            4 - Ordenar actividades
+            5 - Finalizar compra`)
+
+            switch (menuCliente){
+
                 case "1":
-                    costo = calcularCosto(50, cantidadDias, cantidadPersonas)
+                    verActividades (listaActividades)
                 break
+
                 case "2":
-                    costo = calcularCosto(70, cantidadDias, cantidadPersonas)
+                    buscarAct (listaActividades)
                 break
-            }  
+
+                case "3":
+                    agregarActividad (listaActividades)
+                break
+                
+                case "4":
+                    ordenarActividades (listaActividades)
+                break
+
+                case "5":
+                    alert(`¡Muchas gracias por haber confiado en nuestra agencia de turismo!`)
+                break
+
+                default:
+                    console.log(`Opción no válida`)
+                break
+            }
             break
-        case "2":
-            console.log("América del Norte y del Sur")
-            pais = mostrarPaises('Brasil', 'Estados Unidos')
-            switch (pais) {
-                case "1":
-                    costo = calcularCosto(40, cantidadDias, cantidadPersonas)
-                break
-                case "2":
-                    costo = calcularCosto(80, cantidadDias, cantidadPersonas)
-                break
-            }  
-            break
+
         case "3":
-            console.log("Europa")
-            pais = mostrarPaises('España', 'Grecia')
-            switch (pais) {
-                case "1":
-                    costo = calcularCosto(90, cantidadDias, cantidadPersonas)
-                    break
-                case "2":
-                    costo = calcularCosto(70, cantidadDias, cantidadPersonas)
-                break
-            }  
-            break
-        case "0":
-            console.log("Salir Menú")
-            alert(`¡Muchas gracias por utilizar nuestra página!`)
+            alert ("Muchas gracias por utilizar nuestra página de turismo!")
             salirMenu = false
-            break
+        break
+
         default:
-            console.log("Opción no válida")
-            break
+            alert ("Opción no válida. Por favor, seleccione una nueva opción")
+        break
+        } 
     }
-} while (salirMenu)
+    while (salirMenu) 
+}
+menu ()
